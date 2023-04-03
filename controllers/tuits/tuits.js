9a65cc7653431660c44efbadfb162ea0b988da24
Melanie Becker <becker.mel@northeastern.edu>
2023-04-02 23:38:04 -0400

    fixed tuits AGAIN

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,7 +1,7 @@
 export default [
 {
  "_id": "123", "topic": "Space", "username": "SpaceX",
- "handle": "@spacex", "time": "2h", "image": "tesla-logo.png",
+ "handle": "@spacex", "time": "2h", "image": "spacex.png",
  "title": "SpaceX's Mission",
  "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
  "liked": true, "likes": 2345, "replies": 123, "retuits": 432
@@ -15,7 +15,7 @@ export default [
 },
 {
  "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
- "handle": "@neurolink", "time": "3h", "image": "neurolink.jpg",
+ "handle": "@neurolink", "time": "3h", "image": "tesla-logo.png",
  "title": "Neurolink fixes paralisis and blindness",
  "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
  "liked": true, "likes": 3456, "replies": 345, "retuits": 654
